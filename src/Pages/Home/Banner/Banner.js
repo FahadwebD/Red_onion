5146c9deb53ad09eb1633808d04fd5dbb7804075
FahadwebD <fahadchowdhury66779@gmail.com>
2021-12-03 02:34:50 +0600

    polished Banner as psd

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -7,18 +7,19 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 const Banner = () => {
     const element = <FontAwesomeIcon icon={faSearch} />
     return (
-        
-            <Card className="bg-white text-black">
+ <Card className="bg-white text-black w-100">
   <Card.Img src={banner} alt="Card image" />
   <Card.ImgOverlay>
-  <InputGroup className="  " style={{top:'50%' , left:'35%' , width:'30%'}}>
+  <h4 style={{position:'relative' , top:'40%'}}>Best Food Waiting For Your Belly</h4>
+  <InputGroup className=" mb-5 " style={{top:'41%' , left:'39%' , width:'20%'}}>
+     
     <FormControl
       placeholder="food category"
       aria-label="Recipient's username"
       aria-describedby="basic-addon2"
       style={{borderRadius: '15px 15px 15px 15px' , position:'relative', border:'none'}}
     />
-    <Button style={{width:'15%',backgroundColor:'#f91944',border:'none' , borderRadius: '15px 15px 15px 15px' ,left:'90%' , position:'absolute' }}>
+    <Button style={{width:'20%',backgroundColor:'#f91944',border:'none' , borderRadius: '15px 15px 15px 15px' ,left:'90%' , position:'absolute' }}>
       {element}
     </Button>
   </InputGroup>
